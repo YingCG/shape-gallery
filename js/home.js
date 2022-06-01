2de3c42e74eae2c037d11ff81a9de87226a5113f
YingCG <yingchiugoh@gmail.com>
2022-06-01 19:41:07 +1200

    vanilla js

--- js/home.js
+++ js/home.js
@@ -1,22 +1,22 @@
 
-function showTech(){
-    let text = document.getElementById('showData')
-    text.innerHTML = '<h3> Technology is most often used to improve efficiency, it has made art more accessible. Artist can teach us about how to make technology more human. </h3>'
-}
-function showArt(){
-    let text = document.getElementById('showData')
-    text.innerHTML = '<h3> Artists explore the emotional and social elements, with innovative thinking, artist can now make art in digital or on paper. </h3>'
-}
-function showHuman(){
-    let text = document.getElementById('showData')
-    text.innerHTML = '<h3> The ability to imagine and to take that imagination and make it into reality is one of the things that is really distinctive about humans. </h3>'
-}
+// function showTech(){
+//     let text = document.getElementById('showData')
+//     text.innerHTML = '<h3> Technology is most often used to improve efficiency, it has made art more accessible. Artist can teach us about how to make technology more human. </h3>'
+// }
+// function showArt(){
+//     let text = document.getElementById('showData')
+//     text.innerHTML = '<h3> Artists explore the emotional and social elements, with innovative thinking, artist can now make art in digital or on paper. </h3>'
+// }
+// function showHuman(){
+//     let text = document.getElementById('showData')
+//     text.innerHTML = '<h3> The ability to imagine and to take that imagination and make it into reality is one of the things that is really distinctive about humans. </h3>'
+// }
 
-document.getElementById("tech").addEventListener("click", showText(event));
-document.getElementById("art").addEventListener("click", showText(event));
-document.getElementById("human").addEventListener("click", showText(event));
+// document.getElementById("tech").addEventListener("click", showText);
+// document.getElementById("art").addEventListener("click", showText);
+// document.getElementById("human").addEventListener("click", showText);
 
-function showText(){
+function showText(event){
     let text = document.getElementById("showData")
     let target = event.target
     if(target.id === 'tech'){
@@ -28,42 +28,41 @@ function showText(){
     }
 }
 
-function fetchShowData(){
-    fetch('../data/content.json')
-    .then((res) => res.json())
-    .then((jsonData) => {
-        const showDataDiv = document.getElementById('showData')
-        showDataDiv.innerHTML = '';
-        jsonData.forEach((info) => {
-            showDataDiv.innerHTML += `<h3>${info.title}</h3>${info.url}`
-        })
-    })
-}
+// function fetchShowData(){
+//     fetch('../data/content.json')
+//     .then((res) => res.json())
+//     .then((jsonData) => {
+//         const showDataDiv = document.getElementById('showData')
+//         showDataDiv.innerHTML = '';
+//         jsonData.forEach((info) => {
+//             showDataDiv.innerHTML += `<h3>${info.title}</h3>${info.url}`
+//         })
+//     })
+// }
 
-function showImage(){
-    const img = document.getElementById('galleryImg')
-    img.classList.add('img')
-    img.src= `../images/${info.img}`
-
-    img.addEventListener('click', () => {
-        for (let i = 0; i < jsonData.length; i++){
-            img.src= `../images/${info.img} + ${id}`
-            img ++
-        }
-    })
-}
+let contentData;
+let contentIndex = -1;
 
 function fetchInfo(){
     fetch('../data/content.json')
     .then((res) => res.json())
     .then((jsonData) => {
-        const showDataDiv = document.getElementById('showData')
-        const showImageDiv = document.getElementById('showImage')
-        
-        showDataDiv.innerHTML = '';
-        jsonData.forEach((info) => {
-            showDataDiv.innerHTML += `<h3>${info.title}</h3> ${info.url}`
-            showImageDiv.innerHTML += showImage()
-        })
+        contentData = jsonData;
     })
+}
+
+fetchInfo();
+
+function changeInfo(){
+    if(contentIndex === contentData.length) { 
+        contentIndex = 0;
+    }
+    contentIndex++;
+
+    const showDataDiv = document.getElementById('showData')
+    const showImageDiv = document.getElementById('showImage')
+
+    showDataDiv.innerHTML = `<h3>${contentData[contentIndex].title}</h3> ${contentData[contentIndex].url}`
+    showImageDiv.innerHTML = `<img src="${contentData[contentIndex].img}">`;
+    // return img
 }
